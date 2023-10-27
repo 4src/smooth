d17804e5da98e2fbc038eb5572066ea247ad0e6c
Tim Menzies <timm@ieee.org>
2023-10-26 23:49:13 -0400

    try

--- eg.lua
+++ eg.lua
@@ -4,12 +4,15 @@ local the, help = smo.the, smo.help
 
 local eg = {}
 
+local _print = print
+print = function(...) if not the.quiet then _print(...) end end
+
 local function try(s, fun)
   math.randomseed(the.seed)
-  io.write("⭐️ ".. s)
+  io.write("⭐️ ".. s.." ")
   if fun()==false then
-    print(" ❌ FAIL"); return true 
-  else print(" ✅ ") end
+    io.write("❌ FAIL\n"); return true
+  else io.write("✅ \n") end end
 
 local function run()
   l.cli(the)
@@ -23,10 +26,11 @@ function eg.all(     oops)
   oops = -1 -- we have one test that deliberately fails
   for k,fun in l.items(eg) do
     if k~="all" then 
-      if fails(k,fun) then oops = oops + 1 end end end
+      if try(k,fun) then oops = oops + 1 end end end
   l.rogues()
   os.exit(oops) end
 
 function eg.fails() return false end
+function eg.the()   print(l.o(the)) end
 ----------------------------------------------------------------
 run()
